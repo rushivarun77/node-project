8a98cffa9c53223dfd481d5ed5cfcc90569928a4
rushivarun77 <22h51a0592@cmrcet.ac.in>
2024-10-17 23:11:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, Docker!');
 });
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
